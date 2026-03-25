e71e893a6b581fa51800434ac3ef3bddb040cac9
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2026-03-25 20:01:09 +0400

    Add files via upload

---
+++ iJava/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iJava
+//
+//  Created by Bacho on 23.02.26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iJava/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  iJava
+//
+//  Created by Bacho on 23.02.26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
